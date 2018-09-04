aed5e598418756a8aa50f0d4d4563a7b5c27e2fa
joaquim.moura <joaquim.moura@interaconsultoria.com.br>
2018-09-04 11:01:06 -0300

    Tratamento padrão para cada tipo de erro.

--- src/interceptors/error-interceptors.ts
+++ src/interceptors/error-interceptors.ts
@@ -2,11 +2,12 @@ import { Injectable } from "@angular/core";
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Observable } from "rxjs/Rx";
 import { StorageService } from "../services/storege.service";
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
 
-    constructor(public storage: StorageService){
+    constructor(public storage: StorageService, public alertCtrl: AlertController){
 
     }
 
@@ -26,8 +27,17 @@ export class ErrorInterceptor implements HttpInterceptor{
             }
 
             switch(errorOBJ.status){
+
+                case 401:
+                this.handle401();
+                break;
+
                 case 403:
                 this.handle403();
+                break;
+
+                default:
+                this.handleDefaultError(errorOBJ);
             }
 
             console.log("Erro Detectado pelo interceptor: ");
@@ -39,6 +49,37 @@ export class ErrorInterceptor implements HttpInterceptor{
 
     handle403(){
         this.storage.setLocalUser(null);
+    }
+
+    handle401(){
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401:  Falha de autenticação',
+            message: 'Email ou Senha Inválidos',
+
+            // opcional obriga o usuaria a clicar no botão de sair
+            // caso contrario ele pode clicar em qualquer area da tela e sair
+            enableBackdropDismiss: false,
+            buttons: [{
+                text: 'OK'
+            }]
+        });
+        alert.present();
+    }
+
+    handleDefaultError(errorOBJ){
+
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' +  errorOBJ.status + ' : ' + errorOBJ.error,
+            message: errorOBJ.message,
+            // opcional obriga o usuaria a clicar no botão de sair
+            // caso contrario ele pode clicar em qualquer area da tela e sair
+            enableBackdropDismiss: false,
+            buttons: [{
+                text: 'OK'
+            }]
+        });
+        alert.present();
+
     }
 }
 
