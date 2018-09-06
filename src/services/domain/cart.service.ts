69122cef483174777a1732f33d57b13496a25a5f
joaquim.moura <joaquim.moura@interaconsultoria.com.br>
2018-09-06 10:45:30 -0300

    Terminando as funcionalidades do carrinho

--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -33,4 +33,48 @@ export class CartService {
         this.storage.setCart(cart);
         return cart;
     }
+
+    removeProduto(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if (position != -1) {
+            cart.items.splice(position, 1);
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    oncreaseQuantity(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if (position != -1) {
+            cart.items[position].quantidade++;
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    decreaseQuantity(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        if (position != -1) {
+            cart.items[position].quantidade--;
+            if(cart.items[position].quantidade < 1){
+                cart = this.removeProduto(produto);
+            }
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    total() : number {
+
+        let cart = this.getCart();
+        let sum = 0;
+        for (let index = 0; index < cart.items.length; index++) {
+            // multiplica preco pela quantidade
+           sum += cart.items[index].produto.preco * cart.items[index].quantidade;            
+        }
+        return sum;
+    }
 }
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
 import { Observable } from "rxjs/Rx";
-import { ProdutDTO } from "../../models/produto.dto";
+import { ProdutoDTO } from "../../models/produto.dto";
 
 @Injectable()
 export class ProdutoService {
@@ -11,7 +11,7 @@ export class ProdutoService {
     }
 
     findById(produto_id : string){
-        return this.http.get<ProdutDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
     }
 
     findByCategoria(categoria_id : string){
