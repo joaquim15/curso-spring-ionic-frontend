12bcee488d3be8de9810cf0442760d916555c1db
joaquim.moura <joaquim.moura@interaconsultoria.com.br>
2018-09-10 18:49:18 -0300

    Infinite scroll.

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -11,41 +11,46 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class ProdutosPage {
 
-  items : ProdutoDTO[];
+  items: ProdutoDTO[] = [];
+  page: number = 0;
 
-  constructor(public navCtrl: NavController, 
-              public navParams: NavParams, 
-              public produtoService: ProdutoService, 
-              public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
-    this.loadDate();
+    this.loadData();
   };
 
-  loadDate(){
+  loadData() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id).subscribe(response => {
-      this.items = response['content'];
+    this.produtoService.findByCategoria(categoria_id, this.page, 10).subscribe(response => {
+      let start = this.items.length;
+      this.items = this.items.concat(response['content']);
+      let end = this.items.length - 1;
       loader.dismiss();
-      this.loadImageUrls();
+      console.log(this.page);
+      console.log(this.items);
+      this.loadImageUrls(start, end);
     }, error => {
       loader.dismiss();
     });
   }
 
-  loadImageUrls() {
-    for (var i=0; i<this.items.length;i++){
+  loadImageUrls(start: number, end: number) {
+    for (var i = start; i <= end; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id).subscribe(response => {
         item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`
-      }, error =>{});
+      }, error => { });
     }
   }
 
-  showDetail(produto_id : string){
-    this.navCtrl.push('ProdutoDetailPage', { produto_id : produto_id });
+  showDetail(produto_id: string) {
+    this.navCtrl.push('ProdutoDetailPage', { produto_id: produto_id });
   }
 
   presentLoading() {
@@ -57,10 +62,19 @@ export class ProdutosPage {
   }
 
   doRefresh(refresher) {
-    this.loadDate();
+    this.page = 0;
+    this.items = [];
+    this.loadData();
     setTimeout(() => {
       refresher.complete();
     }, 1000);
   }
 
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      infiniteScroll.complete();
+    }, 1000);
+  }
 }
