ff2244ee790f6aa762e0c57b13e6e29c459bfc21
Joaquim de Castro <joaquim_castro_moura@hotmail.com>
2018-09-19 10:44:51 -0300

    Bugfix - atualizacao da imagem de perfil.

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
-import { StorageService } from '../../services/storege.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CameraOptions, Camera } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
+import { StorageService } from '../../services/storege.service';
 
 @IonicPage()
 @Component({
@@ -15,13 +16,18 @@ export class ProfilePage {
 
   cliente: ClienteDTO;
   picture: string;
+  profileImage;
   cameraOn: boolean = false;
 
-  constructor(public navCtrl: NavController,
+  constructor(
+    public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
     public clienteService: ClienteService,
-    public camera: Camera) {
+    public camera: Camera,
+    public sanitizer: DomSanitizer) {
+
+    this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -35,21 +41,40 @@ export class ProfilePage {
         .subscribe(response => {
           this.cliente = response as ClienteDTO;
           this.getImageIfExists();
-
-        }, error => {
-          if (error.status == 403) {
-            this.navCtrl.setRoot('HomePage');
-          }
-        });
-    } else {
+        },
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          });
+    }
+    else {
       this.navCtrl.setRoot('HomePage');
     }
   }
 
   getImageIfExists() {
-    this.clienteService.getImageFromBucket(this.cliente.id).subscribe(response => {
-      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
-    }, error => { });
+    this.clienteService.getImageFromBucket(this.cliente.id)
+      .subscribe(response => {
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+        this.blobToDataURL(response).then(dataUrl => {
+          let str: string = dataUrl as string;
+          this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+        });
+      },
+        error => {
+          this.profileImage = 'assets/imgs/avatar-blank.png';
+        });
+  }
+
+  // https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
+  blobToDataURL(blob) {
+    return new Promise((fulfill, reject) => {
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = (e) => fulfill(reader.result);
+      reader.readAsDataURL(blob);
+    })
   }
 
   getCameraPicture() {
@@ -60,8 +85,7 @@ export class ProfilePage {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.PNG,
-      mediaType: this.camera.MediaType.PICTURE,
-      correctOrientation: true
+      mediaType: this.camera.MediaType.PICTURE
     }
 
     this.camera.getPicture(options).then((imageData) => {
@@ -70,7 +94,6 @@ export class ProfilePage {
     }, (err) => {
       this.cameraOn = false;
     });
-
   }
 
   getGalleryPicture() {
@@ -82,8 +105,7 @@ export class ProfilePage {
       sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.PNG,
-      mediaType: this.camera.MediaType.PICTURE,
-      correctOrientation: true
+      mediaType: this.camera.MediaType.PICTURE
     }
 
     this.camera.getPicture(options).then((imageData) => {
@@ -92,22 +114,19 @@ export class ProfilePage {
     }, (err) => {
       this.cameraOn = false;
     });
-
   }
 
   sendPicture() {
     this.clienteService.uploadPicture(this.picture)
       .subscribe(response => {
         this.picture = null;
-        this.loadData();
-
-      }, error => {
-
-      });
+        this.getImageIfExists();
+      },
+        error => {
+        });
   }
 
   cancel() {
     this.picture = null;
   }
-
 }
